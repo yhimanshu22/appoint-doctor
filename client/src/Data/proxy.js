8b985fc11db1e0748c51a4940bfd0dfcf45e44b4
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-07-14 04:25:14 +0530

    proxy update

--- client/src/Data/proxy.js
+++ client/src/Data/proxy.js
@@ -1,4 +1,4 @@
 const rootUrl = process.env.NODE_ENV === 'production' ? 
-   'https://appoint-doctor-l7ln.onrender.com' : '  http://localhost:3000:8080'
+   'https://appoint-doctor-l7ln.onrender.com' : '  http://localhost:8080'
 
 export default rootUrl;   
