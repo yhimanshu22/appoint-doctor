a03576f721e3bee560f76559fffafa2daa1fd0f1
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-07-14 04:41:02 +0530

    updated

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const port = process.env.PORT || 8080;
 //listen port
 app.listen(port, () => {
   console.log(
-    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+    `Server Running in ${process.env.NODE_ENV} Mode on port ${process.env.PORT}`
       .bgCyan.white
   );
 });
